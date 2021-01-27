f193b6617be803df22f2b873b12fced36c7c1c9f
Stephenalexngo <51846517+Stephenalexngo@users.noreply.github.com>
2021-01-27 15:07:12 +0800

    added custom error

--- customfiles/CustomErrorListener.java
+++ customfiles/CustomErrorListener.java
@@ -6,24 +6,42 @@ import org.antlr.v4.runtime.RecognitionException;
 
 import java.util.ArrayList;
 
-
 public class CustomErrorListener extends BaseErrorListener {
 
     private ArrayList<String> error_List = new ArrayList<>();
 
     public CustomErrorListener() {
+
+    }
+
+    private String CustomErrorMessage(String msg, int line, int pos, String offendingSymbol) {
+        String error_message_temp;
+        
+        if(msg.contains("mismatched input"))
+            error_message_temp = "Unexpected symbol: '" + offendingSymbol + "' in line " + line + ", at pos " + pos;
+        else if (msg.contains("extraneous input"))
+            error_message_temp = "Invalid Token '" + offendingSymbol + "' in line " + line + ", at pos " + pos;
+        else if (msg.contains("no viable alternative at"))
+            error_message_temp = "Syntax Error '" + offendingSymbol + "' in line " + line + ", at pos " + pos;
+        else if(msg.contains("missing "))
+            error_message_temp = "Missing Token in line " + line + ", at pos " + pos;
+        else 
+            error_message_temp = "Syntax Error in line " + line + ", at pos " + pos;
+
+        return error_message_temp;
     }
 
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
             String msg, RecognitionException e) {
-
-        String error_Message = msg + " at line " + line;
+        
+        String error_Message = CustomErrorMessage(msg, line, charPositionInLine, offendingSymbol.toString());
+            
         error_List.add(error_Message);
 
         System.out.println(error_Message);
     }
-
+    
     public int getErrorList(){
         return this.error_List.size();
     }
